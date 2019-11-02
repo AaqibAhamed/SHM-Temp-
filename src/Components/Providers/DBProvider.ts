b794d8626346549b7c536caa21cee6ac34552c55
Darshanapriya K.A.D.S.R. it17050104 <it17050104@my.sliit.lk>
2019-11-02 17:31:53 +0530

    Code Refactoring

--- src/Components/Providers/DBProvider.ts
+++ src/Components/Providers/DBProvider.ts
@@ -1,16 +1,17 @@
 import firebase from "firebase";
 
+firebase.initializeApp({
+    apiKey: "AIzaSyC4w_uz8deOLRoeTPuk-yaap923G7S67S4",
+    authDomain: "shm-sl.firebaseapp.com",
+    databaseURL: "https://shm-sl.firebaseio.com",
+    projectId: "shm-sl",
+    storageBucket: "shm-sl.appspot.com",
+    messagingSenderId: "968800732280",
+    appId: "1:968800732280:web:b7ad6d231767c35950198d",
+    measurementId: "G-CVR4D5J5HG"
+});
+
 export default class DBProvider{
-    private static db = firebase.initializeApp({
-        apiKey: "AIzaSyC4w_uz8deOLRoeTPuk-yaap923G7S67S4",
-        authDomain: "shm-sl.firebaseapp.com",
-        databaseURL: "https://shm-sl.firebaseio.com",
-        projectId: "shm-sl",
-        storageBucket: "shm-sl.appspot.com",
-        messagingSenderId: "968800732280",
-        appId: "1:968800732280:web:b7ad6d231767c35950198d",
-        measurementId: "G-CVR4D5J5HG"
-    });
     public static readonly TABLE_COMPANY = "company";
     public static readonly TABLE_FEATURES = "features";
     public static readonly TABLE_SEO = "seo";
